3ae7d1364c292ce8df3a8d550fbaf0fe0667fb21
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-08-15 22:22:01 +0530

    Adding new contact and editing the contact details

--- src/main/java/com/BridgeLab/AddressBookMain.java
+++ src/main/java/com/BridgeLab/AddressBookMain.java
@@ -5,11 +5,34 @@ import java.util.Scanner;
 
 public class AddressBookMain {
 
+        ArrayList<Contacts> contactDetails = new ArrayList<>();
+        static Scanner sc = new Scanner(System.in);
+
+
         public static void main(String[] args) {
-            System.out.println("Welcome to Address Book System");
-            System.out.println("*******************************");
+            int i=0;
             AddressBookMain addContact=new AddressBookMain();
-            addContact.addContactDetails();
+
+            while (i == 0) {
+                System.out.println("Do you want add new Contact/Edit existing contact: ");
+                System.out.println("1.Add details.\n2.Edit details.");
+                int choose = sc.nextInt();
+                switch (choose) {
+                    case 1:
+                        addContact.addContactDetails();
+                        break;
+                    case 2:
+                        addContact.editContactDetails();
+                        break;
+                    default:
+                        i = 1;
+                        System.out.println("Wrong option");
+                        addContact.addContactDetails();
+                        break;
+                }
+            }
+
+
         }
 
         /***
@@ -35,8 +58,68 @@ public class AddressBookMain {
             details.setZip(sc.nextInt());
             System.out.println("Enter the Phone number of person:");
             details.setPhoneNumber(sc.nextInt());
-            sc.close();
+            //sc.close();
+            contactDetails.add(details);
             System.out.println("*******************************");
             System.out.println(details.toString());
         }
+
+        /***
+         *Method to edit the contact details
+         */
+        public void editContactDetails() {
+
+                System.out.println("Confirm your first name to edit details: ");
+                String name = sc.next();
+
+                for (int i = 0; i < contactDetails.size(); i++) {
+                    if (contactDetails.get(i).getFirstname().equals(name)) {
+                        System.out.println("Select form below to change: ");
+                        System.out.println(
+                                "\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Email\n7.Zip\n8.Phone number");
+                        int e = sc.nextInt();
+
+                        switch (e) {
+                            case 1:
+                                System.out.println("Enter first name");
+                                contactDetails.get(i).setFirstname(sc.next());
+                                break;
+                            case 2:
+                                System.out.println("Enter Last name");
+                                contactDetails.get(i).setLastname(sc.next());
+                                break;
+                            case 3:
+                                System.out.println("Enter address");
+                                contactDetails.get(i).setAddress(sc.next());
+                                break;
+                            case 4:
+                                System.out.println("Enter city");
+                                contactDetails.get(i).setCity(sc.next());
+                                break;
+                            case 5:
+                                System.out.println("Enter state");
+                                contactDetails.get(i).setState(sc.next());
+                                break;
+                            case 6:
+                                System.out.println("Enter email");
+                                contactDetails.get(i).setEmail(sc.next());
+                                break;
+                            case 7:
+                                System.out.println("Enter Zip");
+                                contactDetails.get(i).setZip(sc.nextInt());
+                                break;
+                            case 8:
+                                System.out.println("Enter phone number");
+                                contactDetails.get(i).setPhoneNumber(sc.nextInt());
+                                break;
+                        }
+
+                        System.out.println(contactDetails);
+
+                    } else
+                        System.out.println("Enter valid First name");
+                }
+
+            }
+
 }
